d34a9a3288dca814c6ebc155bc711d0f3f46f914
dkvo <dangkhoanvo@gmail.com>
2018-05-24 02:49:00 -0700

    created a mock calculator

---
+++ app.js
@@ -0,0 +1,23 @@
+dataController = (function() {
+    var result;
+    return {
+
+    };
+})();
+
+
+
+UIController = (function() {
+    return {
+
+    };
+})();
+
+
+appController = (function(dataCtrl, UICtrl) {
+
+    return {
+        
+    }
+})(dataController, UIController);
+
